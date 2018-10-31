188a855473fb7bc9a087e0b9e45f755920176f54
William Dale <williambdale@gmail.com>
2018-10-31 13:55:24 -0400

    Add memoized recursive fib

--- fib/index.js
+++ fib/index.js
@@ -8,11 +8,56 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+function memoize(fn) {
+  const cache = []
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args]
+    }
 
+    const result = fn.apply(this, args);
+    cache[args] = result
+
+    return result
+  }
 }
 
+function slowFib(n) {
+  if (n < 2) {
+    return n
+  }
+
+  return fib(n-1) + fib(n-2)
+}
+
+const fib = memoize(slowFib)
+
 module.exports = fib;
+
+// MINE (REFACTORED RECURSIVE)
+// function fib(n) {
+//   return n < 2 ? n : fib(n-1) + fib(n-2)
+// }
+
+// THEIRS (ITERATIVE)
+// function fib(n) {
+//   const result = [0,1]
+//
+//   for (i=2; i<=n; i++) {
+//     result.push(result[i-1] + result[i-2])
+//   }
+//
+//   return result[n]
+// }
+
+// THEIRS (RECURSIVE)
+// function fib(n) {
+//   if (n < 2) {
+//     return n
+//   }
+//
+//   return fib(n-1) + fib(n-2)
+// }
 
 // MINE (RECURSIVE)
 // 1141.79999 milliseconds for fib(40)
