c4e115d7d58c3305dda2f4bc37f9f1a5ead67d01
William Dale <williambdale@gmail.com>
2018-10-31 14:17:35 -0400

    Solve queue problem

--- queue/index.js
+++ queue/index.js
@@ -10,8 +10,17 @@
 
 class Queue {
   constructor() {
-    
+    this.data = []
   }
+
+  add(n) {
+    this.data.unshift(n)
+  }
+
+  remove() {
+    return this.data.pop()
+  }
+
 }
 
 module.exports = Queue;
