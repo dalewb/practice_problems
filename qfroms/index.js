5620d6cfa2f0bb3191ede21c592d039bc6ab2e9e
William Dale <williambdale@gmail.com>
2018-11-12 19:05:06 -0500

    Create queue using 2 stacks

--- qfroms/index.js
+++ qfroms/index.js
@@ -14,6 +14,119 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.first = new Stack();
+    this.second = new Stack();
+  }
+
+  add(record) {
+    this.first.push(record)
+  }
+
+  remove() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+
+    const record = this.second.pop();
+
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+
+    return record
+  }
+
+  peek() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+
+    const record = this.second.peek();
+
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+
+    return record
+  }
+}
 
 module.exports = Queue;
+
+// MINE
+//
+// class Queue {
+//   constructor() {
+//   this.stackOne = new Stack
+//     this.stackTwo = new Stack
+//   }
+//
+//   switchStack(s1, s2) {
+//     let currentPop = s1.pop()
+//     while (currentPop) {
+//       s2.push(currentPop)
+//       currentPop = s1.pop()
+//     }
+//   }
+//
+//   add(record) {
+//     this.stackOne.push(record)
+//   }
+//
+//   remove() {
+//     this.switchStack(this.stackOne, this.stackTwo)
+//     let popped = this.stackTwo.pop()
+//     this.switchStack(this.stackTwo, this.stackOne)
+//     return popped
+//   }
+//
+//   peek() {
+//     this.switchStack(this.stackOne, this.stackTwo)
+//     let peeked = this.stackTwo.peek()
+//     this.switchStack(this.stackTwo, this.stackOne)
+//     return peeked
+//   }
+// }
+
+// THEIRS
+//
+// class Queue {
+//   constructor() {
+//     this.first = new Stack();
+//     this.second = new Stack();
+//   }
+//
+//   add(record) {
+//     this.first.push(record)
+//   }
+//
+//   remove() {
+//     while (this.first.peek()) {
+//       this.second.push(this.first.pop());
+//     }
+//
+//     const record = this.second.pop();
+//
+//     while (this.second.peek()) {
+//       this.first.push(this.second.pop());
+//     }
+//
+//     return record
+//   }
+//
+//   peek() {
+//     while (this.first.peek()) {
+//       this.second.push(this.first.pop());
+//     }
+//
+//     const record = this.second.peek();
+//
+//     while (this.second.peek()) {
+//       this.first.push(this.second.pop());
+//     }
+//
+//     return record
+//   }
+// }
