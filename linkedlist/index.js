c7c90c82dfe9f056bf07336b9c679515c78af5b2
William Dale <williambdale@gmail.com>
2018-11-14 15:23:26 -0500

    Attempt removeAt()

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -43,7 +43,7 @@ class LinkedList {
     return prevNode
   }
 
-// THEIRS
+// Theirs
   // getLast() {
   //   if (!this.head) {
   //     return null
@@ -79,7 +79,7 @@ class LinkedList {
   //   this.head = null
   // }
 
-// THEIRS
+// Theirs
   // clear() {
   //   this.head = null;
   // }
@@ -88,7 +88,7 @@ class LinkedList {
     this.head = this.head.next
   }
 
-// THEIRS
+// Theirs
   // removeFirst() {
   //   if (!this.head) {
   //     return;
@@ -131,28 +131,145 @@ class LinkedList {
     }
     previous.next = null;
   }
+// Mine
+  // insertLast(data) {
+  //   if (!this.head) {
+  //     this.head = new Node(data)
+  //     return
+  //   }
+  //   let node = this.head
+  //   while (node.next) {
+  //     node = node.next
+  //   }
+  //   node.next = new Node(data)
+  // }
+// Mine (second)
+  // insertLast(data) {
+  //   if (!this.head) {
+  //     this.head = new Node(data)
+  //   }
+  //   let node = this.getLast()
+  //   if (node) {
+  //     node.next = new Node(data)
+  //   }
+  // }
 
+// Theirs
   insertLast(data) {
+    const last = this.getLast();
+    if (last) {
+      last.next = new Node(data);
+    } else {
+      this.head = new Node(data);
+    }
+  }
+
+// Mine
+  // getAt(index, nodeToGet) {
+  //   let count = 0
+  //   let node = this.head
+  //   while (count !== index && node) {
+  //     count++
+  //     node = node.next
+  //   }
+  //   return node
+  // }
+
+// Mine (second)
+  // getAt(index) {
+  //   if (!this.head) {
+  //     return
+  //   }
+  //   let count = 0
+  //   let node = this.head
+  //   while (node) {
+  //     if (count === index) {
+  //       return node
+  //     }
+  //     count++
+  //     node = node.next
+  //   }
+  //   return node
+  // }
+
+// Theirs
+  getAt(index) {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      if (counter === index) {
+        return node
+      }
+
+      counter++
+      node = node.next
+    }
+    return null
+  }
+
+// Mine
+  // removeAt(index) {
+  //   if (!this.head) {
+  //     return
+  //   }
+  //   if (index === 0) {
+  //     this.head = this.head.next
+  //   }
+  //   let count = 0
+  //   let node = this.head
+  //   let prevNode = null
+  //   while (node && index !== count) {
+  //     count++
+  //     prevNode = node
+  //     node = node.next
+  //   }
+  //
+  //   prevNode.next = prevNode.next.next
+  // }
+
+// Mine (second)
+  // removeAt(index) {
+  //   if (!this.head) {
+  //     return
+  //   } else if (index === 0 && this.head.next) {
+  //     this.head = this.head.next
+  //     return
+  //   } else if (index === 0) {
+  //     this.head = null
+  //     return
+  //   }
+  //   let node = this.head;
+  //   let prevNode = null
+  //   let count = 0;
+  //   while (node) {
+  //     if (count === index) {
+  //       prevNode.next = node.next
+  //       return
+  //     }
+  //     count++
+  //     prevNode = node
+  //     node = node.next
+  //   }
+  // }
+
+  removeAt(index) {
     if (!this.head) {
-      this.head = new Node(data)
+      return;
+    }
+
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+
+    const previous = this.getAt(index - 1);
+    if (previous.next) {
+      previous.next = previous.next.next
       return
     }
-    let node = this.head
-    while (node.next) {
-      node = node.next
-    }
-    node.next = new Node(data)
+    return null
   }
 
-  getAt(index, nodeToGet) {
-    let count = 0
-    let node = this.head
-    while (count !== index && node) {
-      count++
-      node = node.next
-    }
-    return node
-  }
 }
 
 module.exports = { Node, LinkedList };
