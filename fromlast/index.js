417bd50bfd3dc7ab16a5581bcb1fe1491c5cc38f
William Dale <williambdale@gmail.com>
2018-11-15 14:48:37 -0500

    Finish official fromlast linkedlist function

--- fromlast/index.js
+++ fromlast/index.js
@@ -11,6 +11,24 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
+// Theirs
+function fromLast(list, n) {
+  let slow = lixt.getFirst();
+  let fast = list.getFirst();
+
+  while ( n > 0) {
+    fast = fast.next;
+    n--;
+  }
+
+  while(fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  return slow;
+}
+
 // Mine
 function fromLast(list, n) {
   let first = list.head
